6e435bf4fba4fe80f7cc7338421be5241d4882ef
kjj9677 <kjj9677@naver.com>
2022-11-01 11:27:04 +0900

    frontend - modify color of component

--- frontend/pages/plans/index.tsx
+++ frontend/pages/plans/index.tsx
@@ -2,7 +2,7 @@ import { FC, useEffect, useState } from "react";
 import Link from "next/link";
 import styled from "@emotion/styled";
 import axios from "axios";
-import { getPeriodText, toAuthorizetionHeader } from "../../utils";
+import { toAuthorizetionHeader } from "../../utils";
 import { BASE_API_URI } from "../../const";
 import Button from "../../src/components/Button";
 import PlanInfo from "../../src/components/PlanInfo";
@@ -30,18 +30,9 @@ const Plans: FC = () => {
   if (isLoading) {
     return <div />;
   }
+
   return (
-    <div
-      style={{
-        alignItems: "center",
-        display: "flex",
-        flexDirection: "column",
-        height: "100vh",
-        justifyContent: "center",
-        rowGap: 30,
-        width: "100vw",
-      }}
-    >
+    <PlansContainer>
       <Button color="white">여행 계획표 목록</Button>
       {planList &&
         planList.map(({ id, destination, period, title }) => (
@@ -53,14 +44,23 @@ const Plans: FC = () => {
             title={title}
           />
         ))}
-
       <Link href="/creation">
         <a>
           <Button color="white">+ 새 계획표 만들기</Button>
         </a>
       </Link>
-    </div>
+    </PlansContainer>
   );
 };
 
 export default Plans;
+
+const PlansContainer = styled.div({
+  alignItems: "center",
+  display: "flex",
+  flexDirection: "column",
+  height: "100vh",
+  justifyContent: "center",
+  rowGap: 30,
+  width: "100vw",
+});
